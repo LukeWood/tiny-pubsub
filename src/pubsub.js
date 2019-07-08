41ee7a78450887cd0f77c7bfcf37a2e62c1b3b9c
Luke Wood <lukewoodcs@gmail.com>
2019-07-07 23:23:01 -0700

    This commit adds tests and they all pass

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,25 +1,30 @@
-const subscriptions = Object.create(null);
+let subscriptions = Object.create(null);
 
 function subscribe(evt, func) {
   if(typeof func !== 'function') {
     throw "Subscribers must be functions"
   }
-  const oldSubscriptions = Object.getOwnProperty.call(subscriptions, evt) || [];
+  const oldSubscriptions = subscriptions[evt] || [];
   oldSubscriptions.push(func);
   subscriptions[evt] = oldSubscriptions;
 }
 
-function publish(evt, ...args) {
-  const subFunctions = Object.getOwnProperty.call(subscriptions, evt) || [];
+function publish(evt) {
+  let args = Array.prototype.slice.call(arguments, 1);
+  const subFunctions = subscriptions[evt] || [];
   for(let i = 0; i < subFunctions.length; i++) {
-    subFunctions[i].apply(args)
+    subFunctions[i].apply(null, args)
   }
 }
 
 function unsubscribe(evt, func) {
-  const oldSubscriptions = Object.getOwnProperty.call(subscriptions, evt) || [];
+  const oldSubscriptions = subscriptions[evt] || [];
   const newSubscriptions = oldSubscriptions.filter((item) => item !== func);
   subscriptions[evt] = newSubscriptions;
 }
 
-export {subscribe, publish, unsubscribe}
+function cancel(evt) {
+  delete subscriptions[evt];
+}
+
+module.exports={subscribe, publish, unsubscribe, cancel}
---
+++ test/tests.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const {subscribe, publish, unsubscribe, cancel} = require('../src/pubsub')
+
+let valueSet = false;
+function setTrue() {
+  valueSet = true;
+}
+function setFalse() {
+  valueSet = false;
+}
+
+describe('pubsub', () => {
+  describe('subscribe', () => {
+    it('should call subscribed functions', () => {
+      setFalse();
+      subscribe('should-set', setTrue)
+      publish('should-set')
+      assert.equal(valueSet, true)
+    })
+
+    it('should call subscribed functions with arguments', () => {
+      setFalse();
+      subscribe('should-set', (arg) => {
+        if(arg == 1){
+          setTrue();
+        }
+      })
+      publish('should-set', 1)
+      assert.equal(valueSet, true)
+
+      setFalse();
+      publish('should-set', 2)
+      assert.equal(valueSet, false)
+    })
+
+    it('should unsusbscribe functions', () => {
+      setFalse();
+      subscribe('should-set', setTrue)
+      publish('should-set')
+      assert.equal(valueSet, true)
+
+      setFalse();
+      unsubscribe('should-set', setTrue)
+      publish('should-set')
+      assert.equal(valueSet, false)
+    })
+
+    afterEach(() => {
+      cancel('should-set');
+    })
+  })
+})
