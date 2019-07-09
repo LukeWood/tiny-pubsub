1e918e3a0f76b2287efe5bca0677eef99ddb6483
Luke Wood <lukewoodcs@gmail.com>
2019-07-08 23:15:36 -0700

    reintroduce es style, I am stupid

--- pubsub.js
+++ pubsub.js
@@ -1,30 +1,25 @@
-var subscriptions = {};
+let subscriptions = Object.create(null);
 
 function subscribe(evt, func) {
   if(typeof func !== 'function') {
     throw "Subscribers must be functions"
   }
-  var oldSubscriptions = subscriptions[evt] || [];
+  const oldSubscriptions = subscriptions[evt] || [];
   oldSubscriptions.push(func);
   subscriptions[evt] = oldSubscriptions;
 }
 
 function publish(evt) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  var subFunctions = subscriptions[evt] || [];
-  for(var i = 0; i < subFunctions.length; i++) {
+  let args = Array.prototype.slice.call(arguments, 1);
+  const subFunctions = subscriptions[evt] || [];
+  for(let i = 0; i < subFunctions.length; i++) {
     subFunctions[i].apply(null, args)
   }
 }
 
 function unsubscribe(evt, func) {
-  var oldSubscriptions = subscriptions[evt] || [];
-  var newSubscriptions = [];
-  for(var i = 0; i < oldSubscriptions.length; i++) {
-    if (oldSubscriptions[i] != func) {
-      newSubscriptions.push(oldSubscriptions[i]);
-    }
-  }
+  const oldSubscriptions = subscriptions[evt] || [];
+  const newSubscriptions = oldSubscriptions.filter((item) => item !== func);
   subscriptions[evt] = newSubscriptions;
 }
 
@@ -32,4 +27,4 @@ function cancel(evt) {
   delete subscriptions[evt];
 }
 
-module.exports = {subscribe: subscribe, publish: publish, unsubscribe: unsubscribe, cancel: cancel}
+export {subscribe, publish, unsusbscribe, cancel}
