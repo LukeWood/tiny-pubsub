e65b195f67b6008f611d9280e07a1064cd86db42
Luke Wood <lukewoodcs@gmail.com>
2019-07-08 22:57:33 -0700

    es2015 friendly

--- pubsub.js
+++ pubsub.js
@@ -1,25 +1,25 @@
-let subscriptions = Object.create(null);
+var subscriptions = {};
 
 function subscribe(evt, func) {
   if(typeof func !== 'function') {
     throw "Subscribers must be functions"
   }
-  const oldSubscriptions = subscriptions[evt] || [];
+  var oldSubscriptions = subscriptions[evt] || [];
   oldSubscriptions.push(func);
   subscriptions[evt] = oldSubscriptions;
 }
 
 function publish(evt) {
-  let args = Array.prototype.slice.call(arguments, 1);
-  const subFunctions = subscriptions[evt] || [];
-  for(let i = 0; i < subFunctions.length; i++) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var subFunctions = subscriptions[evt] || [];
+  for(var i = 0; i < subFunctions.length; i++) {
     subFunctions[i].apply(null, args)
   }
 }
 
 function unsubscribe(evt, func) {
-  const oldSubscriptions = subscriptions[evt] || [];
-  const newSubscriptions = oldSubscriptions.filter((item) => item !== func);
+  var oldSubscriptions = subscriptions[evt] || [];
+  var newSubscriptions = oldSubscriptions.filter((item) => item !== func);
   subscriptions[evt] = newSubscriptions;
 }
 
@@ -27,4 +27,4 @@ function cancel(evt) {
   delete subscriptions[evt];
 }
 
-export {subscribe, publish, unsusbscribe, cancel}
+module.exports  ={subscribe, publish, unsubscribe, cancel}
